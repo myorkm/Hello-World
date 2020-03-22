4a95edc7bd5e11a2bfc47cfd5016a0d4123d7d76
Manuel Marcigliano <manuel.marcigliano@web.de>
2020-03-22 18:38:19 +0200

    Projektdateien hinzufügen.

---
+++ Hello-World/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+
+            if (args.Length > 0)
+                output = args[0];
+
+            Console.WriteLine($@"Hello World!{output}");
+        }
+    }
+}
